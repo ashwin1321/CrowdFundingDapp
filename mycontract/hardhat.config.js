6732abf321eae312b842f7d4d042c649d5454be3
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-22 20:58:40 +0545

    smart contract deployed

--- mycontract/hardhat.config.js
+++ mycontract/hardhat.config.js
@@ -1,7 +1,17 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+const env = require("dotenv").config();
+
 module.exports = {
   solidity: {
-    version: '0.8.9',
+    version: "0.8.9",
+    defaultNetwork: "goerli",
+    networks: {
+      hardnat: {},
+      goerli: {
+        url: "https://rpc.ankr.com/eth_goerli",
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
