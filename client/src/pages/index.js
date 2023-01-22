7790323d77f073d603677ddce2da95f45765166c
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-22 21:50:11 +0545

    necessary files created

---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home";
+export { default as CreateCamp } from "./CreateCamp";
+export { default as Profile } from "./Profile";
+export { default as CampaignDetails } from "./CampaignDetails";
