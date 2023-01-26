853dc1fdef090141cc84397f4edd23dc56e5aecf
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-26 21:49:46 +0545

    create campaign page created

--- client/src/pages/CreateCamp.jsx
+++ client/src/pages/CreateCamp.jsx
@@ -1,7 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ethers } from "ethers";
+import { money } from "../assets";
+import { CustomButton } from "../components";
+import { checkIfImage } from "../utils";
+import { FormField } from "../components";
 
 const CreateCamp = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [form, setForm] = useState({
+    name: "",
+    title: "",
+    description: "",
+    target: "",
+    deadline: "",
+    image: "",
+  });
+
+  const handleFormFieldChange = (fieldName, e) => {
+    setForm({ ...form, [fieldName]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+  return (
+    <div className="bg-[#1c1c24] flex flex-col justify-center items-center rounded-[10px] sm:p-10 p-4">
+      {isLoading && "Loading..."}
+      <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]">
+        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">
+          Start a Campaign
+        </h1>
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        className="w-full mt-[65px] flex flex-col gap-[30px]"
+      >
+        <div className="flex flex-wrap gap-[40px]">
+          <FormField
+            labelName="Your Name *"
+            inputType="text"
+            placeholder="Enter your name"
+            value={form.name}
+            handleChange={(e) => handleFormFieldChange("name", e)}
+          />
+          <FormField
+            labelName="Campaign Title *"
+            inputType="text"
+            placeholder="Enter the title name"
+            value={form.title}
+            handleChange={(e) => handleFormFieldChange("title", e)}
+          />
+        </div>
+        <FormField
+          labelName="Story *"
+          inputType="text"
+          isTextArea
+          placeholder="Write your story"
+          value={form.description}
+          handleChange={(e) => handleFormFieldChange("description", e)}
+        />
+
+        <div className="w-full flex justify-start items-center p-4 bg-[#8c6dfd] h-[120px] rounded-[10px]">
+          <img
+            src={money}
+            alt="money"
+            className="w-[40px] h-[40px] object-contain"
+          />
+          <h4 className="font-epilogue font-bold text-[25px] text-white ml-[20px]">
+            You will get the 100% of the raised amount.
+          </h4>
+        </div>
+
+        <div className="flex flex-wrap gap-[40px]">
+          <FormField
+            labelName="Goal *"
+            inputType="text"
+            placeholder="ETH 0.50"
+            value={form.target}
+            handleChange={(e) => handleFormFieldChange("target", e)}
+          />
+          <FormField
+            labelName="End Date *"
+            inputType="date"
+            placeholder="End Date"
+            value={form.deadline}
+            handleChange={(e) => handleFormFieldChange("deadline", e)}
+          />
+        </div>
+        <FormField
+          labelName="campaign Image *"
+          inputType="url"
+          placeholder="Place image url of your campaign"
+          value={form.image}
+          handleChange={(e) => handleFormFieldChange("image", e)}
+        />
+
+        <div className="flex justify-center items-center mt-[40px]">
+          <CustomButton
+            btnType="submit"
+            title="submit new campaign"
+            styles="bg-[#1dc071]"
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CreateCamp;
