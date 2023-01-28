56a6c83b515723e377c72a2f8502b04475aaae7c
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-28 23:01:28 +0545

    Home page loading

---
+++ client/src/components/DisplayCampaigns.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DisplayCampaigns = () => {
+  return <div></div>;
+};
+
+export default DisplayCampaigns;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,6 @@
+
+export { default as DisplayCampaigns } from "./DisplayCampaigns";
+
 export { default as Sidebar } from "./Sidebar";
 
 export { default as Navbar } from "./Navbar";
@@ -5,3 +8,4 @@ export { default as Navbar } from "./Navbar";
 export { default as CustomButton } from "./CustomButton";
 
 export { default as FormField } from "./FormField";
+
